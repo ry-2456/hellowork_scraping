63d1acd06f97ae399707d3b03b823d41c16f7e13
ry-2456 <ittanmomennimakaretyaimashita@gmail.com>
2020-06-23 00:48:31 +0900

    scrape occupations

--- hello_work_scraping.py
+++ hello_work_scraping.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-	
-import csv
+import csv
 import pprint
 from urllib import request
 from bs4 import BeautifulSoup
@@ -8,9 +8,12 @@ from browser_manipulator import *
 
 # フルタイム
 # 全国・未経験可・ソフトウェア
-# 大阪兵庫.京都
 # 職種　技術職 ソフトウェア開発技術者
 
+def show_length_and_elems(l):
+    print("length: {}".format(len(l)))
+    for i, e in enumerate(l):
+        print(i, e)
 
 def scraping(html):
     # set BeautifulSoup インスタンスの作成
@@ -22,50 +25,70 @@ def scraping(html):
     kyujin_foots = soup.find_all("tr", attrs={"class", "kyujin_foot"})
 
     # kyujin_footsを詳細ボタンを持っているものに絞り込む
-    kyujin_foots = [elem for elem in kyujin_foots if elem.find("a") is not None]
+    # kyujin_foots = [elem for elem in kyujin_foots if elem.find("a") is not None]
 
-    occupations = []   # 職種
-    companies = []     # 会社の名前
-    offer_numbers = [] # 求人番号
-    locations = []     # 勤務地
+    occupations = []     # 職種
 
-    # urls = []          # 会社のホームページURL
+    job_division = []    # 求人区分
+    companies = []       # 会社の名前
+    locations = []       # 就業場所
+    job_description = [] # 仕事の内容
+    emp_style = []       # 雇用形態
+    payment = []         # 賃金
+    work_time = []       # 就業時間
+    day_off = []         # 休日
+    age_limit = []       # 年齢
+    offer_numbers = []   # 求人番号
+
 
     # 職種を取得
     for head in kyujin_heads:
         occupations.append(head.find("tr").find_all("td")[1].div.string)
+    show_length_and_elems(occupations)
+    
 
     # 会社名, 求人番号を取得
-    for body in kyujin_bodys:
-        body_row = body.find_all("tr")
-        for i in range(len(body_row)):
-            table_data = body_row[i].find_all("td")  # 全テーブルデータを取得
-            row_name = table_data[0].string          # データの名称を取得
-            if row_name is None: continue
-            if row_name == "事業所名":
-                companies.append(table_data[1].div.string)
-            elif row_name == "求人番号":
-                offer_numbers.append(table_data[1].div.string)
-
-    # 事業所名を公開していない会社を取り除く
-    for i, comp_name in enumerate(companies):
-        if "公開していません" in comp_name:
-            occupations[i] = None
-            companies[i] = None
-            offer_numbers[i] = None
+    # for body in kyujin_bodys:
+    #     body_row = body.find_all("tr")
+    #     for i in range(len(body_row)):
+    #         table_data = body_row[i].find_all("td")  # 全テーブルデータを取得
+    #         row_name = table_data[0].string          # データの名称を取得
+    #         if row_name is None: continue
+    #         if row_name == "求人区分":
+    #             job_division.append(table_data[0].div.string)
+    #             print("求人区分: {}".format(table_data[0].div.string))
+    #         elif row_name == "事業所名":
+    #             companies.append(table_data[1].div.string)
+    #             print("事業所名: {}".format(table_data[0].div.string))
+    #         elif row_name == "就業場所":
+    #             companies.append(table_data[1].div.string)
+    #             print("就業場所: {}".format(table_data[0].div.string))
+    #         elif row_name == "仕事の内容":
+    #             companies.append(table_data[1].div.text)
+    #             print("仕事の内容: {}".format(table_data[0].div.string))
+    #         # elif row_name == "雇用形態":
+    #         #     companies.append(table_data[1].div.string)
+    #         # elif row_name == "賃金":
+    #         #     companies.append(table_data[1].div.string)
+    #         # elif row_name == "休日":
+    #         #     companies.append(table_data[1].div.string)
+    #         # elif row_name == "年齢":
+    #         #     companies.append(table_data[1].div.string)
+    #         # elif row_name == "求人番号":
+    #         #     offer_numbers.append(table_data[1].div.string)
 
     # Noneを削除
-    occupations = [elem for elem in occupations if elem is not None]
-    companies = [elem for elem in companies if elem is not None]
-    offer_numbers = [elem for elem in offer_numbers if elem is not None]
+    # occupations = [elem for elem in occupations if elem is not None]
+    # companies = [elem for elem in companies if elem is not None]
+    # offer_numbers = [elem for elem in offer_numbers if elem is not None]
 
-    print("after")
-    print("len(occupations): {}".format(len(occupations)))
-    print("len(companies): {}".format(len(companies)))
-    print("len(offer_numbers): {}".format(len(offer_numbers)))
+    # print("after")
+    # print("len(occupations): {}".format(len(occupations)))
+    # print("len(companies): {}".format(len(companies)))
+    # print("len(offer_numbers): {}".format(len(offer_numbers)))
     
     # return (companies, offer_numbers)
-    return (companies, occupations, offer_numbers)
+    # return (companies, occupations, offer_numbers)
 
 def read_html(full_path):
     "full_pathで指定された、ファイルを読み込みその中身を返す"
@@ -97,6 +120,7 @@ def main(full_path):
         del_tab()
         # スクレイピングを行う
         comp_names, occups, offer_nums = scraping(html)
+        break
         # csvファイルに書き込み
         write_joboffer_info(full_path, comp_names, occups, offer_nums)
         # 次のページヘ
@@ -110,15 +134,18 @@ def test(full_path):
         # htmlのページを閉じる
         del_tab()
         # スクレイピングを行う
-        comp_names, occups, offer_nums = scraping(html)
+        # comp_names, occups, offer_nums = scraping(html)
+        scraping(html)
+        break
         # csvファイルに書き込み
         write_joboffer_info(full_path, comp_names, occups, offer_nums)
         # 次のページへ
         go_next_page()
         time.sleep(1)
 
+
 if __name__ == "__main__":
-    file_path = "/home/t-rin/brother_project/new_2020_0421/"
+    file_path = "/home/t-rin/brother_project/hello_work_scraping/"
     file_name = "joboffer_info.csv"
-    # test(file_path + file_name)
-    main(file_path + file_name)
+    test(file_path + file_name)
+    # main(file_path + file_name)
